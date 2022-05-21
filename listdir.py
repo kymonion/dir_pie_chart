8c503687ea275137f48f45aa1a32c4c71e4c6e97
khyup <102271583+khyup@users.noreply.github.com>
2022-05-22 00:19:46 +0900

    added print filesize feature

--- listdir.py
+++ listdir.py
@@ -1,5 +1,15 @@
 import os
 
+def get_dir_size(path='.'):
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
+
 dirs = []
 files = []
 
@@ -12,9 +22,11 @@ for item in os.listdir(os.getcwd()):
 print("\n-= 폴더 =-")
 for item in dirs:
     print(item)
+    print("size : %s" % get_dir_size(item))
 
 print("\n-= 파일 =-")
 for item in files:
     print(item)
+    print("size : %s" % os.path.getsize(item))
 
 input("\n-= Enter로 종료 =-")
